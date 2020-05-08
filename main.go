def28a89438cda291f25e5c1e6d7ae11da4a582b
leofanky <fillow99@gmail.com>
2020-05-08 11:30:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+     "encoding/json"
+     "fmt"
+     "log"
+     "net/http"
 )
 
 type Simple struct {
-	Name string
-	Description string
-	Url string
+     Name string
+     Description string
+     Url string
+}
+
+func SimpleFactory (host string) Simple {
+     return Simple{"Hello", "World", host}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	simple := Simple{"Hello", "World",r.Host}
-
-	jsonOutput, _ := json.Marshal(simple)
-
-	fmt.Fprintln(w, string(jsonOutput))
+     simple := SimpleFactory(r.Host)
+     jsonOutput, _ := json.Marshal(simple)
+     fmt.Fprintln(w, string(jsonOutput))
+     // fmt.Fprintln(w, simple)
 }
 
 func main() {
-	fmt.Println("Server started")
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":9090", nil))
+     fmt.Println("Server started")
+     http.HandleFunc("/", handler)
+     log.Fatal(http.ListenAndServe(":8080", nil))
 }
