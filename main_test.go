12cdf660dd242207e52a0b6a7f1f091d3f438066
leofanky <fillow99@gmail.com>
2020-05-08 11:25:41 +0200

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -2,10 +2,9 @@ package main
 
 import "testing"
 
-
 func TestSimpleFactory(t *testing.T) {
     f := SimpleFactory("http://localhost")
     if f.Url != "http://localhost" {
-      t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localh
+        t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localh
     }
 }
